e43d560f25d72ca9955042526da0c96550cb82f0
qkxgit <44601074+qkxgit@users.noreply.github.com>
2020-12-29 09:33:45 +0800

    更新目录结构

--- src/thread/KAny.cpp
+++
@@ -1,4 +0,0 @@
-#include "thread/KAny.h"
-namespace klib {
-	
-};
--- src/thread/KEventObject.cpp
+++
@@ -1,7 +0,0 @@
-#include "thread/KEventObject.h"
-
-namespace klib {
-	KMutex KEventBase::s_eobjmtx;
-	AtomicInteger<uint32_t> KEventBase::s_eobjid;
-	std::map<uint32_t, KEventBase*> KEventBase::s_eobjmap;
-};
